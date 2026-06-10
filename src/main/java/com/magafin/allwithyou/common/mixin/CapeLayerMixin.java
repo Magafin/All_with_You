f84792964cdf815a25b484f27953bf1a30a3021f
Magafin <magafiny@gmail.com>
2026-06-10 09:39:20 +0300

    Cape Render fix, texture improvements

---
+++ src/main/java/com/magafin/allwithyou/common/mixin/CapeLayerMixin.java
@@ -0,0 +1,32 @@
+package com.magafin.allwithyou.common.mixin;
+
+import com.magafin.allwithyou.common.register.ItemsReg;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.layers.CapeLayer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CapeLayer.class)
+public class CapeLayerMixin {
+
+    @Inject(
+            method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void allwithyou$hideCapeIfBackpackEquipped(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        // Проверяем, что надето в слоте груди
+        ItemStack chestItem = player.getItemBySlot(EquipmentSlot.CHEST);
+
+        // Если это наш рюкзак — отменяем рендер плаща
+        if (chestItem.is(ItemsReg.BACKPACK.get())) {
+            ci.cancel();
+        }
+    }
+}
